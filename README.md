06d6d4df3cfc0e8066032f5268c3008f5b5bc1d6
Jun Hashimoto <jun.hashimoto.0204@gmail.com>
2014-07-24 22:45:35 +0900

    add 1.0.0 github.commitdiff.user.js

---
+++ README.md
@@ -0,0 +1,29 @@
+github_commitdiff_viewer
+========================
+
+----
+## what is github\_commit\_viewer?
+
+It's a user script which enables to access commit diff compare page easily.
+
+----
+## how to install
+
+### Google Chrome
+
+On Chrome, install [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=ja), then access github.commitdiff.user.js.
+
+### Safari
+
+On Safari, install [NinjaKit](http://os0x.hatenablog.com/entry/20100612/1276330696), then access github.commitdiff.user.js
+
+----
+## usage
+Access your repository commits page( such as https://github.com/manji602/github_commitdiff_viewer/commits/master ).
+If you install this user.js, there are radio buttons on each commits.
+Choose 'from' and 'to' radiobuttons, then click 'Commit Diff' button on upper right.
+Then, you can access commit compare page.
+
+----
+## author
+Jun HASHIMOTO
---
+++ github.commitdiff.user.js
@@ -0,0 +1,74 @@
+// ==UserScript==
+// @name           Github UserScript
+// @namespace      Jun HASHIMOTO
+// @include        https://github.com/*
+// @version        1.0.0
+// @require http://code.jquery.com/jquery-2.1.1.min.js
+// ==/UserScript==
+
+var buttonArea = $('.pagehead-actions:first');
+
+var makeDiffButton = function() {
+    var diffButton = $('<li><a href="#" class="button minibutton showDiffLink">Show Diff</a></li>');
+    diffButton.bind("click", showDiffLink);
+    buttonArea.append(diffButton);
+};
+
+var makeRadioButtons = function() {
+    var commitAreas = $('.commit-group-item');
+    commitAreas.each(function(){
+        var commitLink = $(this).find('.zeroclipboard-button:first');
+        var commitHash = commitLink.attr('data-clipboard-text');
+        var fromRadioButton = makeRadioButton('commitFrom', 'from', commitHash);
+        var toRadioButton   = makeRadioButton('commitTo', 'to', commitHash);
+        $(this).append(fromRadioButton);
+        $(this).append(toRadioButton);
+    });
+};
+
+var makeRadioButton = function(propertyName, textName, commitHash){
+    return $('<span><input type="radio" name=' + propertyName + ' value=' + commitHash + '></input>' + textName + '</span>');
+};
+
+var showDiffLink = function() {
+    var fromCommit = getCommitHash('commitFrom');
+    var toCommit   = getCommitHash('commitTo');
+    if (fromCommit && toCommit) {
+        var currentUrl      = location.href;
+        var commitUrlHeader = getCommitUrl(currentUrl);
+        var commitUrl       = commitUrlHeader + fromCommit + '...' + toCommit;
+        location.href       = commitUrl;
+    }
+};
+
+var getCommitHash = function(className) {
+    var radioButtonAttribute = 'input[name=' + className + ']:checked';
+    return $(radioButtonAttribute).attr('value');
+};
+
+var getCommitUrl = function(currentUrl) {
+    return currentUrl.replace(/commits\/(\S+)/, 'compare/');
+};
+
+var hasDiffButton = function() {
+    var diffButton = document.getElementsByClassName('showDiffLink');
+    return ( diffButton.length === 0 ) ? false : true;
+};
+
+var onLocationChange = function(callback) {
+    var previous = '';
+    setInterval(function(e) {
+        if (previous != location.href) {
+            previous = location.href;
+            callback(location);
+        }
+    }, 1000);
+};
+
+// main function
+onLocationChange( function(location) {
+    if( location.pathname.match(/\/commits\//) && !hasDiffButton() ){
+        makeDiffButton();
+        makeRadioButtons();
+    }
+});
